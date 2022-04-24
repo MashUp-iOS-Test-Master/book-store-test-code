15cec8394d1d6627da3daa697eb56564e6dca679
Eddy <kimkyunghun3@naver.com>
2022-04-24 23:54:46 +0900

    feat: tableview 사용하기 위한 초기 설정 구현

---
+++ book-store/Controller/BookDetailTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BookDetailTableViewCell.swift
+//  book-store
+//
+//  Created by Eddy on 2022/04/24.
+//
+
+import UIKit
+
+final class BookDetailTableViewCell: UITableViewCell {
+    @IBOutlet private weak var title: UILabel!
+    @IBOutlet private weak var category: UILabel!
+    @IBOutlet private weak var publicationDate: UILabel!
+    @IBOutlet private weak var price: UILabel!
+    
+}
--- book-store/Controller/BookListViewController.swift
+++ book-store/Controller/BookListViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class BookListViewController: UIViewController {
+final class BookListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
 }
 
