f4aca76bdd0814471b4e5334087aee8596e2c2e3
Eddy <kimkyunghun3@naver.com>
2022-04-25 20:56:11 +0900

    feat: UserDefault 구현 및 카테고리 선택 구현

---
+++ book-store/Controller/BookDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BookDetailViewController.swift
+//  book-store
+//
+//  Created by Eddy on 2022/04/25.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    @IBOutlet private weak var bookTitle: UITextField!
+    @IBOutlet private weak var publicationDate: UITextField!
+    @IBOutlet private weak var price: UITextField!
+    @IBOutlet private weak var category: UIButton!
+    
+    @IBAction func done(_ sender: UIButton) {
+        let userDefault = UserDefaults.standard
+        
+        userDefault.set(self.bookTitle.text, forKey: "BookTitle")
+        userDefault.set(self.publicationDate, forKey: "PublicationDate")
+        userDefault.set(self.price, forKey: "Price")
+        
+        self.presentingViewController?.dismiss(animated: true)
+    }
+    
+    @IBAction func category(_ sender: Any) {
+        let alertMessage = UIAlertController(title: "원하는 카테고리 선택하세요", message: nil, preferredStyle: .actionSheet)
+        alertMessage.addAction(UIAlertAction(title: "소설", style: .default, handler: setCategoryValue))
+        alertMessage.addAction(UIAlertAction(title: "기술", style: .default, handler: setCategoryValue))
+        alertMessage.addAction(UIAlertAction(title: "경제", style: .default, handler: setCategoryValue))
+        alertMessage.addAction(UIAlertAction(title: "시집", style: .default, handler: setCategoryValue))
+        alertMessage.addAction(UIAlertAction(title: "취소", style: .cancel))
+        
+        present(alertMessage, animated: true)
+    }
+    
+    private func setCategoryValue(action: UIAlertAction) {
+        category.titleLabel?.text = action.title
+    }
+}
