310633cb04fdef55c15981d00dd356e86e13fbad
Eddy <kimkyunghun3@naver.com>
2022-04-30 18:34:33 +0900

    feat: TableViewFooter 초기 세팅 구현

--- book-store/Controller/BookListViewController.swift
+++ book-store/Controller/BookListViewController.swift
@@ -11,8 +11,28 @@ final class BookListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeTableFooterView()
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+    }
+    
+    private func makeTableFooterView() {
+        let footer = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 150))
+        
+        footer.backgroundColor = .clear
+        
+        let footerLabel = UILabel(frame: footer.bounds)
+        footerLabel.text = "가격 합계 : "
+        footerLabel.textAlignment = .center
+        footer.addSubview(footerLabel)
+        
+        tableView.tableFooterView = footer
+    }
+}
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
