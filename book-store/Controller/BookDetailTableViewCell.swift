32cae11b0c8b6eb0afb8566cd48ae32fd841f459
Eddy <kimkyunghun3@naver.com>
2022-05-02 18:05:18 +0900

    feat: 소수점 찍히는 numberFormatter 구현

--- book-store/Controller/BookDetailTableViewCell.swift
+++ book-store/Controller/BookDetailTableViewCell.swift
@@ -13,4 +13,12 @@ final class BookDetailTableViewCell: UITableViewCell {
     @IBOutlet private weak var publicationDate: UILabel!
     @IBOutlet private weak var price: UILabel!
     
+    private func makeNumberFormatter(price: String) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 0
+        let formattedPrice = numberFormatter.string(for: price) ?? "0"
+        
+        return formattedPrice
+    }
 }
